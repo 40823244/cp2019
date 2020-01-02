fe5adb05e32512f6f1376ae3d60ee22647354844
40823244 <s40823244@mde.tw>
2020-01-02 15:52:11 +0800

    test

--- downloads/dart_ex/weight/weight.dart
+++ downloads/dart_ex/weight/weight.dart
@@ -1,20 +1,23 @@
-main() {
-  int len;
-  var type;
-  var number;
-  List temp = [ "25kg", "30lb", "56lb", "14kg", "68lb", "198kg"];
-  for (var data in temp) {
-    len = data.length;
-    type = data[len-2];
-    number = data.substring(0, len-2);
-    number = int.parse(number);
-    if (type == "k"){
-      double type=number*2.2;
-      print("kg:$number, =$type lb");
-    }else{
-      double type=number*0.45;
-      print("lb:$number, =$type kg");
-    }
-      
-  }
-} 
+GtoB(num g){
+return g*2.2046;
+}
+BtoG(num b){
+return b*0.4535;
+}
+main(){
+var type;
+int len;
+var number;
+List weight= ["25kg", "30lb", "56lb", "14kg", "68lb", "198kg"];
+for (var i in weight) {
+len = i.length;
+type = i[len-2];
+number = i.substring(0, len-2);
+number = int.parse(number);
+if (type == "k"){
+print(" $number kg =  ${GtoB(number).toStringAsFixed(4)} lb");
+}else{
+print(" $number lb =  ${BtoG(number).toStringAsFixed(4)} kg");
+}
+}
+}
